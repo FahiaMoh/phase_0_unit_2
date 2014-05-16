e570043e2f2240c0f263527dd2adcc9bee8ded15
Fahia Moh <fahia_mohamed@hotmail.com>
2014-05-16 15:30:50 -0400

    uploading challenge 4 and 5 of week 4

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -11,14 +11,90 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+Inputting an integer value that will come back as a string with a comma if it is greater than 999
+1. create a method called separate_comma that passes num as a parameter
+2. in order to get a string value, set num to a new string value
+3. create an if statement that states if the length of the string value is less than 4 to just return it as a regular string with no Commas
+4. if it is less than 7 digits, return the string value with one comma on the position of -4
+5. for the value of a million, set the string value equal to 7 and return the commas on positions, -7 and -4
+6.  for values less than 10(but greater than 7) return commas on the same positions as above
 
 # 2. Initial Solution
+def separate_comma(num)
+    num_string = num.to_s
+  if num_string.length < 4
+    return num_string
+    elsif num_string.length < 7
+    return num_string.insert(-4,",")
+    elsif num_string.length == 7
+    return num_string.insert(-7,",") && num_string.insert(-4,",")
+    elsif num_string.length < 10
+    return num_string.insert(-7,",") && num_string.insert(-4,",")
+end 
+end 
 
-
+def seperate_comma(num)
+    num_string = num.to_s
+    if num_string < 1000
+        num_string 
+    elsif num_string >= 10000
+        num_string.insert(-4,",")
+    elsif num_string >= 100000
+        num_string.insert(-4,",") && num_string.insert(-4,",")
 
 
 # 3. Refactored Solution
+def separate_comma(num)
+	num_string = num.to_s
+	if num_string.length < 4 
+        num_string 
+    elsif  4<= num_string.length && num_string.length< 7
+        num_string.insert(-4, ",") 
+	elsif 7<= num_string.length && num_string.length <= 10
+    num_string.insert(-7, ",") && num_string.insert(-4, ",") 
+end
+end  
+# I took the returns out of the code because I found that in the if statement it wasn't necessary to have the return put in
+# I also put the comparators on one line instead of splitting it into 3 elsifs i made them into two clearer ones with &&
+# I'm not sure how else I could go  about making it shorter and cleaner
+
+# driver test code 
+separate_comma(123) == "123"  # => true
+separate_comma(123456) == "123,456" # => true
+separate_comma(0)      == "0" 		# => true
+separate_comma(123456789) == "123,456,789" # => true 
 
 
 
 # 4. Reflection 
+# What parts of your strategy worked? What problems did you face?
+# I faced lots of problems in this challenge because I tried to do so many things all at once and it was not working
+# I forgot to change it to a string value so I did a method with ranges like 0..3 and it wasn't coming out right because I was using integers and not string values
+# after taking a break I realized that i had to convert it to a string value and I was overthinking it and just put an if else statement comparing the digits
+
+# What questions did you have while coding? What resources did you find to help you answer them?
+# I tried really hard not to look at anything since this was a solo challenge but I had to peek at the ruby docs multiple times to find something
+# that could help me add a comma into the string value, and I found many things that could possibly work that ultimately failed(push,gsub,etc) and none of these
+# did what I needed them to do and the asked to not use reg exp so I couldn't go about it like that.
+
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+# I think my biggest problem was overthinking this whole challenge, I took it and tried to do too much and it got really frustrating and
+# after taking a long(much needed) break I realized it was a lot more simpler than I thought and I coudl simply put it in an if else statement and use comparators
+
+# Did you learn any new skills or tricks?
+# taking a look at the ruby docs and seeing the many ways to go about this challenge I learned lots of types of regexp that I could utilize
+# for other challenges in the future like gsub or split and other things that affect a string and integers.  I found that the .insert method worked
+# far better than a push method because I could manipulate the positioning of what I was putting into the element and the #push didn't give me that and
+# didn't work on strings whereas the #gsub dind't work on string values 
+
+# How confident are you with each of the Learning Competencies?
+# I think through the struggling I grasped the concepts and really took my time going through this and making sure every little thing was registering
+# and I made sure everything I grasped was concrete
+
+# Which parts of the challenge did you enjoy?
+# I enjoyed the end of the challenge when all my driver tests were coming up as true and I realized my efforts finally brought about 
+# a working functional(not pretty) solution.
+
+# Which parts of the challenge did you find tedious?
+# nothing was tedious, just frustrating and challenging but this wasn't negative. I liked that I was stumped and had to take a break
+# and re-assess the issue. 
--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
@@ -12,14 +12,81 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+# inputting a value to pad the indexes in an array with nil
+# outputting the array + the Nil pads following in succession within the array which is the same number inputted
+# 1. create a class Array 
+# 2. define a method called pad that takes the parameter of num which will be the number that represents the padding in the array
+# and the second parameter would be what would be outputted for the padding which is Nil 
+# 3. create a counter variable that is equal to the num parameter and subtract it from the array you would have called self and its count
+# 4. put a times on the counter so each if the counter is less than 0 return the self that has the padding value pushed into the array 
+# 5. the pad method will share most similarities except there will be a map method on the self and this will equal to arr_destr
+# which will prevent the method from being non-destructive 
 
 # 2. Initial Solution
-
-
-
-
+# class Array
+    
+#   def pad(num, padding = nil)
+#     counter = num - self.count
+#     arr_destr = self.map {|x| x}
+#     counter.times {arr_destr << padding} if counter > 0
+#            arr_destr 
+#     end 
+    
+#     def pad!(num,padding = nil)
+#         counter = num - self.count
+#      if counter > 0
+#         counter.times {self << padding}
+#            self
+#        else self
+#         end
+#     end
+# end 
 # 3. Refactored Solution
+class Array
 
-
+	def pad!(num,padding = nil)
+        counter = num - self.count
+        counter.times {self << padding} if counter > 0
+           self
+    end
+end 
+    
+  def pad(num, padding = nil)
+    counter = num - self.count
+    arr_destr = self.map {|x| x}
+    counter.times {arr_destr << padding} if counter > 0
+           arr_destr 
+    end 
+    
+    
+# put the if statements in one line for both methods
+# not sure what else I can do to compress the code anymore than it is at the moment
 
 # 4. Reflection 
+# What parts of your strategy worked? What problems did you face?
+# I think a significant problem was actually knowing where to start with this particular challenge. I also didn't really know the distinctive differences
+# between destructive and non-destructive methods and I initially made them the same
+# I also had problems getting the last few tests to pass, the padded at the end of array test was a frustrating one and I had to take out my if statement
+# and use the new variable i created using the map method
+# it was frustrating because as far as I coudl tell it was running fine and I had to play aroudn with it and try different things and finally i figured out the 
+# map method worked to pass the destructive test 
+
+# What questions did you have while coding? What resources did you find to help you answer them?
+# I always have the ruby docs open to narrow down what methods would work to help me get to a working solution
+
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+# I solidified my understanding with destructive and non-destructive methods after this exercise because the distinctive differences
+# weren't as black and white as they are now
+# I am also not sure on classes in ruby and need some more light shed on this topic 
+# Did you learn any new skills or tricks?
+# upon refactoring I learned to put the if statements in one line and that the push method didn't work as efficiently as the << method in this case
+
+# How confident are you with each of the Learning Competencies?
+# I think i've fully grasped the learning competencies of this challenge
+
+# Which parts of the challenge did you enjoy?
+# It's always a cool feeling to see all the tests pass with no failures and see that you have a working solution
+
+# Which parts of the challenge did you find tedious?
+# I think for me the tests that weren't passing were frustrating and tedious to figure out why because from my perspective it looked
+# like everything was in working order and it didn't make sense why it wasn't passing 
