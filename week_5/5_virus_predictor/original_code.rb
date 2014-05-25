9cfa110837e6752b1e8e0ca357215a561141e5da
Fahia Moh <fahia_mohamed@hotmail.com>
2014-05-25 09:58:42 -0400

    updated reflections on challenges 5,6 and 10

--- week_5/5_virus_predictor/original_code.rb
+++ week_5/5_virus_predictor/original_code.rb
@@ -1,14 +1,14 @@
 # U2.W5: Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # EXPLANATION OF require_relative
-#
-#
+# allows you load the file relative to the file containing require_relative statement
+# it is useful for files not directly in the path, or hidden away inside a project and not inteded for outside use.
 require_relative 'state_data'
 
 class VirusPredictor
-
+  #ititalizes the viruspredictor class with the following parameters being passed to it.
   def initialize(state_of_origin, population_density, population, region, regional_spread)
     @state = state_of_origin
     @population = population
@@ -17,49 +17,45 @@ class VirusPredictor
     @next_region = regional_spread
   end
 
+
+ #This method runs through the private predicted_deaths and speed_of_spread methods in order to highlight the viruspredictor effects 
+  #The scope of the instance variables is the instance objects.  
   def virus_effects  #HINT: What is the SCOPE of instance variables?
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
   end
 
   private  #what is this?  what happens if it were cut and pasted above the virus_effects method
+  #private methods can be called only in the context of the current object; you cannot call on another object's private methods.
+  #by moving the private label, the virus_effects method would become private, so it couldn't be called by another user
 
+  #this method predicts deaths based on density.  The higher the density, the more relative deaths
   def predicted_deaths(population_density, population, state)
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+    case @population_density
+    when 0..49 then number_of_deaths = (@population * 0.1).floor
+    when 50..99 then number_of_deaths = (@population * 0.2).floor
+    when 100..149 then number_of_deaths = (@population * 0.3).floor
+    when @population_density >= 200 then number_of_deaths = (@population * 0.4)
     else 
-      number_of_deaths = (@population * 0.05).floor
-    end
-
+      number_of_deaths = (@population * 0.05).floor 
+    end 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-
   end
 
+
+ #this method predicts spread of virus based on density.The higher the density, the quicker the spread
   def speed_of_spread(population_density, state) #in months
     speed = 0.0
-
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
+    case @population_density
+    when 0..49 then speed += 2
+    when 50..99 then speed += 1.5
+    when 100..149 then speed += 1 
+    when 150..200 then speed += 0.5
     else 
-      speed += 2.5
-    end
-
+      speed += 0.5 
+    end 
     puts " and will spread across the state in #{speed} months.\n\n"
-
   end
-
 end
 
 #=======================================================================
@@ -79,3 +75,9 @@ california.virus_effects
 
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population], STATE_DATA["Alaska"][:region], STATE_DATA["Alaska"][:regional_spread]) 
 alaska.virus_effects
+
+# REFLECTION 
+
+# I didn't find the commenting too difficult, I had already had previous knowledge on private/public methods and their purpose.  
+# The refactoring was also not very difficult.  I always try to invoke a case method to try and shorten and simiplify the if/else statements being used
+# if it all possible 
--- week_5/6_validate_credit_card/my_solution.rb
+++ week_5/6_validate_credit_card/my_solution.rb
@@ -5,10 +5,20 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: input the 16 digit credit card number 
+# Output: outputs either true or false depending on whether the card number inputted is valid.  If valid, true, else, false. 
 
+# Steps:
+ # create an itialize method that takes credit_number as a single parameter
+ # set that instance variable equal to credit_number
+ # raise an arguement error if the size is not equal to 16 digits
+ # create a method called check_card
+ # credit_number will equal its instance variable that will return an array of strings.  The map method will return the new array with the changes.
+ # now we need to manipulate the indexes in order to get the luhn algorithm working
+ # for each element in the index we will multiply it by two if the index number is evenly divisible.
+ #now the credit_number variable will equal the array of strings but we will join them back together and return a new array with these changes.
+ # we will now return true or false depending on if the input is a multiple of 10.  We will use the inject method and add the numbers together
+ # if the total number is evenly divisible by 10, then the output will be true, else the outcome will be false.    
 
 # 3. Initial Solution
 
@@ -16,22 +26,38 @@
 # of exactly 16 digits
 class CreditCard
 
-end
-
-
-
-# 4. Refactored Solution
-
-
-
+       def initialize(credit_number)
+           @credit_number = credit_number
+            raise ArgumentError.new("Card number cannot be more or less than 16 digits") if credit_number.to_s.size != 16  
+        end     
+    
+	def check_card
+    	credit_number = @credit_number.to_s.chars.map{|d| d.to_i}
+    	credit_number.each_index do |index|
+    	credit_number[index] = credit_number[index] * 2	if index % 2 == 0 
+    	end
+  		credit_number = credit_number.join.chars.map{|d| d.to_i}
+		return true if credit_number.inject(:+) % 10 == 0
+		return false if credit_number.inject(:+) % 10 != 0 
+	end
+end 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+card_1 = CreditCard.new(4563960122001999)
+card_2 = CreditCard.new(4563960122001991)
+card_3 = CreditCard.new(4563320122003991)
 
-
-
-
+p card_1.check_card == true	
+p card_2.check_card == false 
+p card_3.check_card == false 
 
 
 # 5. Reflection 
+# I found this challenge to be exceptionally hard and I had to do a lot of googling to really grasp the luhn algorithm and figure out how to really go about this challenge
+# I definitely think pairing would've helped the initial confusion of where to start or how to tackle the challenge. 
+# I think i've grasped the concepts for the most part.  
+# The biggest issue for me was grasping the steps 1-3 and translating that into code 
+# I think this was a really hard challlenge, I spent a lot of time working on it and it was really different than the other challenges
+# this week 
